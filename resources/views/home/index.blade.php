bedb8fd70e54cec0981536e32515ad516ee86d0b
aripratama <arim53689@gmail.com>
2018-11-24 12:42:42 +0700

    ari

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-uuu
-=======
 @extends('layout.master')
 @section('judul','Nyervisga? | Selamat Datang')
 
@@ -8,4 +5,3 @@ uuu
     <button class="btn btn-primaty">awwwwwwwwwwwwwwwwwwwww</button>
     <button class="btn btn-succes">sss</button>
 @endsection
->>>>>>> d45b1c2b0d4a0c8a1795dfd27cef3177b8bb9d6c
